15c5b50104ef4acee83a1af3cb93d5c9641834a2
Kwizera Theo <kwizerasinapy49@gmail.com>
2026-07-31 19:18:48 +0200

    HomeController-index Updated

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -11,39 +11,53 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $posts = Post::with([
-         'user',
-         'category',
-         'comments',
-         'tags'
-         ])
-        ->where('status','published')
+            'user',
+            'category',
+            'tags'
+        ])
+        ->where('status', 'published')
 
+        // Search
         ->when($request->filled('search'), function ($query) use ($request) {
-            $search = $request->search;
 
-            $query->where(function ($q) use ($search) {
+        $search = $request->search;
 
-            $q->where('title', 'like', "%{$search}%")
-            ->orWhere('content', 'like', "%{$search}%")
+        $query->where(function ($q) use ($search) {
+
+        $q->where('title', 'like', "%{$search}%")
+            ->orwhere('content', 'like', "%{$search}%")
 
             ->orWhereHas('category', function ($category) use ($search) {
-                $category->where('name', 'like', "%{$search}%");
+                $category->where(
+                    'name',
+                    'like',
+                    "%{$search}%"
+                );
+            })
+
+            ->orWhereHas('user', function ($user) use ($search) {
+                $user->where(
+                    'name',
+                    'like',
+                    "%{$search}%"
+                );
             })
 
             ->orWhereHas('tags', function ($tag) use ($search) {
-                $tag->where('name', 'like', "%{$search}%");
+                $tag->where(
+                    'name',
+                    'like',
+                    "%{$search}%"
+                );
             });
+        });
 
-            });
         })
-
-        ->whereNotNull('slug')
         ->latest()
         ->paginate(6)
         ->withQueryString();
-        // ->get();
-        //return view('frontend.home', compact('posts'));
-         return view('home', compact('posts'));
+
+        return view('home', compact('posts'));
     }
     //
 
